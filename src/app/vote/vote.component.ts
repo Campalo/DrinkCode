27e93d622712530d49938742c336edaca6bd32c0
jenna <jenna_kim1@hotmail.com>
2019-06-15 14:25:46 +0200

    added cubic-bezier for swipe animation

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -9,17 +9,17 @@ import { trigger, keyframes, animate, transition, state, style } from '@angular/
     trigger('swipe', [
       state('right', style({
         opacity: 0,
-        transform: 'rotate(45deg)',
+        transform: 'rotate(30deg)',
         transformOrigin: 'right bottom'
       })),
       state('left', style({
         opacity: 0,
-        transform: 'rotate(-45deg)',
+        transform: 'rotate(-30deg)',
         transformOrigin: 'left bottom'
       })),
       transition('* => right', [
         style({transformOrigin: 'right bottom'}), 
-        animate(300)]),
+        animate('300ms cubic-bezier(.93,.39,.39,1.71)')]),
       transition('* => left', [
         style({transformOrigin: 'left bottom'}), 
         animate(300)]),
