7571b4ce35d2497d95665e146da05f019e0e1cc1
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-06-15 12:59:11 +0200

    Add login page

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private afAuth: AngularFireAuth) { }
+
+  ngOnInit() {
+  }
+
+  loginWithGoogle() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+
+}
