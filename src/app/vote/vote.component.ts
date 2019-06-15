17858889d6f27c7c4e97adb38c34947ace0957bf
jenna <jenna_kim1@hotmail.com>
2019-06-15 10:52:01 +0200

    imported @angular/animations

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import { trigger, keyframes, animate, transition } from '@angular/animations';
+import * as kf from './keyframes';
 
 @Component({
   selector: 'app-vote',
   templateUrl: './vote.component.html',
-  styleUrls: ['./vote.component.css']
+  styleUrls: ['./vote.component.css'],
 })
 export class VoteComponent implements OnInit {
 
