97de519e6a475567eecb6bc198b97abc9ba8d7b4
jenna <jenna_kim1@hotmail.com>
2019-06-15 10:38:06 +0200

    created vote component with mat-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import {AngularFirestoreModule} from '@angular/fire/firestore';
 import {AngularFireStorageModule} from '@angular/fire/storage';
 import {AngularFireAuthModule} from '@angular/fire/auth';
 import {environment} from '../environments/environment';
+import {MatCardModule} from '@angular/material/card';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { VoteComponent } from './vote/vote.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, VoteComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -18,6 +20,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     AngularFireAuthModule,
     AngularFireStorageModule,
     AngularFirestoreModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent],
